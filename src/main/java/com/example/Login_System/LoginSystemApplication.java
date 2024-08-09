3801bb75e2bee877112fbad88b0ee4d233bdcf55
MusialaGOAT <xuanye_wang@ucsb.edu>
2024-08-09 17:29:42 +0800

    initial code

---
+++ src/main/java/com/example/Login_System/LoginSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Login_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Login_System/LoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Login_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
